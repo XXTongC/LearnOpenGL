f5352d8f18f1e6dbd382214ccfd2bb7f2b5d6275
XXTong <2449208757@qq.com>
2024-04-09 16:58:48 +0800

    panda.jpg can't open (solved) texture and putout test

---
+++ shader.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "core.h"
+#include <string>
+namespace GLShaderwork
+{
+	class Shader
+	{
+	public:
+		Shader(const char* vertexPath,const char* fragmentPath);
+		~Shader();
+		GLuint getProgram() const;
+		void setFloat(const std::string& name, float value);
+		void setInt(const std::string& name, int value);
+		void setVector3(const std::string& name, float x, float y, float z);
+		void setVector3(const std::string& name, const float* values);
+
+		void begin();	//开始使用当前shader
+		void end();		//解除当前shader
+	private:
+		GLuint mProgram{ 0 };
+		void checkShaderErrors(GLuint target, const std::string& type);
+	};
+}
